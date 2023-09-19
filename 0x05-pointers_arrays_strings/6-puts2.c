b178cb6db2f1db25404c0cecd904066d09cea161
Kvlr20 <salamifemihope@gmail.com>
2023-09-19 16:11:50 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -19,14 +19,14 @@ void puts2(char *str)
 		y++;
 		longi++;
 	}
-	t = longi = 1;
-	for (o = 0; o <= t ; o++)
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
 	{
 		if (o % 2 == 0)
-		{
-			_putchar(str[o]);
-		}
+	{
+		_putchar(str[o]);
+	}
 	}
 	_putchar('\n');
-
 }
+
